1d51f3f64a4cf7133c930bd8d338bf1e4971b6fa
Nisara Ismath <nisaraismath@gmail.com>
2024-03-18 10:09:57 +0530

    change commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    //Add your origins here
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"]
+  });
+
+
   await app.listen(3000);
 }
 bootstrap();
